31b4108bbf8b5540eb77e72ac19d3b34ffaaa6c3
yuki_system <roadtrainnight-job@yahoo.co.jp>
2019-04-18 15:32:20 +0900

    メニューバーを追加

--- src/main/scala/MyMainFrame.scala
+++ src/main/scala/MyMainFrame.scala
@@ -4,7 +4,20 @@ package MyMainFrame
 import scala.swing._
 
 class MyMainFrame extends MainFrame {
+  frame =>
   title = "GUI Program #1"
   preferredSize = new Dimension(320, 240)
   contents = new Label("Here is the contents!")
+  menuBar = new MenuBar {
+    contents += new Menu("ファイル(F)") {
+      contents += new MenuItem(Action("新規ウィンドウ(N)") { println("新規ウィンドウ") }) {
+        // mnemonic = Key.N
+      }
+      contents += new MenuItem(Action("終了(X)") {
+        frame.dispose()
+      }) {
+        //menumonic = Key.X
+      }
+    }
+  }
 }
